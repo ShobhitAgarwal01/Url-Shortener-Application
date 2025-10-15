08851155f07a5e8f702883b6f3335338bbb3cca4
monk_douluo <agarwal.shobhit1123@gmail.com>
2025-10-15 15:14:10 +0530

    Fourth Commit - Upgrading Packages Accordingly form the Referred Repository

--- url-shortener-frontend/tailwind.config.js
+++ url-shortener-frontend/tailwind.config.js
@@ -1,16 +1,16 @@
- /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 export default {
-   content: ["./index.html","./src/**/*.{js,ts,jsx,tsx}"],
-   theme: {
-     extend: {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
       backgroundImage: {
-        "custom-gradient": "linear-gradient(to right, #3b82f6, #9333ea)",
+        "custom-gradient": "linear-gradient(to right, #3b82f6, #9333ea)", // equivalent to from-blue-500 to-purple-600
         "custom-gradient-2": "linear-gradient(to left, #3b82f6, #f43f5e)",
         "card-gradient": "linear-gradient(to right, #38b2ac, #4299e1)",
       },
       colors: {
         navbarColor: "#ffffff",
-        btnColor: "#3364f7",
+        btnColor: "#3364F7",
         linkColor: "#2a5bd7",
       },
       boxShadow: {
@@ -27,8 +27,8 @@ export default {
    variants: {
     extend: {
       backgroundImage: ["responsive"],
-    }
+    },
    },
 
    plugins: [],
- }
+};
