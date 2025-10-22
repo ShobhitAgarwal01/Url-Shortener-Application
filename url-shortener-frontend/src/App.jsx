02ea13e127024bf38c1fcd4ef37ba554f96aa58f
shobhit <saintyao23@gmail.com>
2025-10-22 12:21:59 +0530

    Fifth Commit - Addition of Routing, Completion of DashboardLayout and LandingPage etc., Graph

--- url-shortener-frontend/src/App.jsx
+++ url-shortener-frontend/src/App.jsx
@@ -1,19 +1,15 @@
-import { useState } from 'react'
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import LandingPage from './components/LandingPage'
-import AboutPage from './components/AboutPage'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { getApps } from './utils/helper'
 
 function App() {
+
+  const CurrentApp = getApps();
+
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<LandingPage />}/>
-          <Route path='/about' element={<AboutPage />}/>
-        </Routes>
-      </BrowserRouter>
-    </>
+    <Router>
+      <CurrentApp />
+    </Router>
   )
 }
 
